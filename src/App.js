a0075bad4b1ad2a9704de5043f4f37b156bc890d
Stefan <stefanbogdanovic030@gmail.com>
2018-10-15 23:30:01 +0200

    Add new vehicle to the list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import Vehicles from "./components/Vehicles";
+import AddVehicle from "./components/AddVehicle";
 
 class App extends Component {
-  state = {
-    vehicles: [
-        {id: 1, name: 'Vehicle 1', speed: 200, range: '230 miles'},
-        {id: 2, name: 'Vehicle 2', speed: 210, range: '230 miles'},
-        {id: 3, name: 'Vehicle 3', speed: 220, range: '230 miles'}
-    ]
-  }
+    state = {
+        vehicles: [
+            {id: 1, name: 'Vehicle 1', speed: 200, range: '230 miles'},
+            {id: 2, name: 'Vehicle 2', speed: 210, range: '230 miles'},
+            {id: 3, name: 'Vehicle 3', speed: 220, range: '230 miles'}
+        ]
+    }
 
+    addVehicle = (vehicle) => {
+        vehicle.id = Math.floor(Math.random() * 100);
+        let vehicles = [...this.state.vehicles, vehicle];
+        this.setState({
+           vehicles: vehicles
+        })
+        console.log(vehicles);
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <Vehicles vehicles={this.state.vehicles} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>React App</h1>
+                <p>Welcome! :)</p>
+                <Vehicles vehicles={this.state.vehicles} />
+                <AddVehicle addVehicle={this.addVehicle}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/AddVehicle.js
@@ -0,0 +1,41 @@
+    import React, { Component } from 'react';
+
+
+class AddVehicle extends Component {
+    state = {
+        name: null,
+        speed: null,
+        range: null
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addVehicle(this.state);
+        console.log('submit');
+    }
+
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Vehicle name:</label>
+                    <input type="text" id="name" onChange={this.handleChange}/>
+                    <label htmlFor="speed">Vehicle top speed:</label>
+                    <input type="text" id="speed" onChange={this.handleChange}/>
+                    <label htmlFor="range">Vehicle range:</label>
+                    <input type="text" id="range" onChange={this.handleChange}/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddVehicle;
